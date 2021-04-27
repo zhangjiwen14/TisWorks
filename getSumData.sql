cbd2374062ab1d7dcc1036bae377f8a5dea205a8
zhangjinwen <54303757+zhangjiwen14@users.noreply.github.com>
2021-04-27 16:31:29 +0900

    Add files via upload

---
+++ getSumData.sql
@@ -0,0 +1,47 @@
+select facility_list from sum_userid_facility_01  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_02  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_03  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_04  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_05  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_06  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_07  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_08  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_09  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_10  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_11  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_12  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_13  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_14  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_15  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_16  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_17  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_18  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_19  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_20  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_21  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_22  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_23  use index(idx_user_id) where user_id ='@user_id@'
+union
+select facility_list from sum_userid_facility_24  use index(idx_user_id) where user_id ='@user_id@'
